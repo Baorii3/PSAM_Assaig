7b7a1dd39126465040ff2d1ff9182fa47aa83f66
Ian <baorii310@gmail.com>
2025-12-02 19:15:24 +0100

    fet scroll i canvi

--- app/src/main/java/com/example/lamevasalut/LoginActivity.kt
+++ app/src/main/java/com/example/lamevasalut/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lamevasalut
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,15 +14,11 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_login)
         initComponents()
         initUI()
         initListeners()
+
     }
 
     private fun initComponents() {
